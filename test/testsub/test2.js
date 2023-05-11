969c29397b260dcf10a1234237402ebaf5b1a77e
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-05-11 11:46:12 +0200

    Ran lint-fix

--- test/testsub/test2.js
+++ test/testsub/test2.js
@@ -1,17 +1,17 @@
-var assert = require('assert');
-//Add --recursive to packagejson to include subfolder tests
+const assert = require('assert');
+// Add --recursive to packagejson to include subfolder tests
 describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-
-        it('should return 2 when the value is at index 2', function () {
-            assert.equal([1, 2, 3].indexOf(3), 2);
-        });
-
-        it.skip('This test is pending', function () {
-            assert.equal([1, 2, 3].indexOf(3), 2);
-        });
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
     });
+
+    it('should return 2 when the value is at index 2', function () {
+      assert.equal([1, 2, 3].indexOf(3), 2);
+    });
+
+    it.skip('This test is pending', function () {
+      assert.equal([1, 2, 3].indexOf(3), 2);
+    });
+  });
 });
